01256546655db65e0e059ee83585eb3585c40d41
JacoTome <jacotomelleri@hotmail.com>
2023-05-20 16:49:19 +0200

    Added some reqFunz and tried 2 query

--- backend/app.js
+++ backend/app.js
@@ -1,76 +1,45 @@
-const express = require('express');
-const SparqlClient = require('sparql-http-client');
-const http = require('http');
+const express = require("express");
+const SparqlClient = require("sparql-http-client");
+const http = require("http");
+const fs = require("fs");
 const app = express();
-const port = 3002
-const jenaFusekiServer = 'http://localhost:3030/';
+const port = 3002;
+const jenaFusekiServer = "http://localhost:3030/";
 
+const client = new SparqlClient({
+  endpointUrl: jenaFusekiServer + "Musico/sparql",
+});
+// Read the contents of the query file
+const SPquery = fs.readFileSync("./queries/query1.sparql", "utf8");
+const SPquery2 = fs.readFileSync("./queries/query2.sparql", "utf8");
+// Use the query variable in your SPARQL client code
 
-app.get('/sendQuery', async(req, res) =>  {
-  // const query = 'PREFIX%20owl%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0APREFIX%20foaf%3A%20%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0APREFIX%20rdf%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0APREFIX%20rdfs%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0APREFIX%20musico%3A%20%3Chttp%3A%2F%2Fpurl.org%2Fontology%2Fmusico%23%3E%0APREFIX%20smi%3A%20%3Chttp%3A%2F%2Fpurl.org%2Fontology%2Fiomust%2Fsmi%3E%0APREFIX%20mo%3A%20%3Chttp%3A%2F%2Fpurl.org%2Fontology%2Fmo%2F%3E%0APREFIX%20iomust%3A%20%3Chttp%3A%2F%2Fpurl.org%2Fontology%2Fiomust%2Finternet_of_things%3E%0APREFIX%20ex%3A%20%3Chttp%3A%2F%2Fwww.example.audio%2F%3E%0ASELECT%20DISTINCT%20%3Fentity1%20%3Fpred%20%3Fobj%0AWHERE%20%7B%0A%20%20%3Fentity1%20%3Fpred%20%3Fobj%3B%0A%20%20%20%20%20%20%20%20%20%20%20musico%3Aplays_genre%20ex%3APop%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20foaf%3AfirstName%20%22Atai%22%20.%0A%7D%20%0A%0AORDER%20BY%20%3Finstrument%0ALIMIT%20100';
-  // const options = {
-  //   hostname: 'localhost',
-  //   port: 3030,
-  //   path: '/Musico/sparql' ,
-  //   method: 'POST',
-  //   body: {
-  //     payload: query,
-  //   },
-  //   headers: {
-  //     'Accept': 'application/sparql-results+json',
-  //     'Connection': 'keep-alive',
-  //     'Content-Type': 'application/x-www-form-urlencoded',
-  //     'Content-Length': query.length
-  //     },
-  //   }
-  //
-  // const request = http.request(options, (response) => {
-  //   console.log(`STATUS: ${response.statusCode}`);
-  //   console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-  //   response.setEncoding('utf8');
-  //   response.on('data', (chunk) => {
-  //     console.log(`BODY: ${chunk}`);
-  //     res.send(chunk);
-  //   });
-  //   });
-  // request.on('error', (e) => {
-  //   console.log(`problem with request: ${e.message}`);
-  //   res.send(e.message);
-  // });
-  // request.end();
-
-  const client = new SparqlClient({ endpointUrl: jenaFusekiServer + 'Musico/sparql' });
-  const query = `PREFIX owl: <http://www.w3.org/2002/07/owl#>
-  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX musico: <http://purl.org/ontology/musico#>
-  PREFIX smi: <http://purl.org/ontology/iomust/smi>
-  PREFIX mo: <http://purl.org/ontology/mo/>
-  PREFIX iomust: <http://purl.org/ontology/iomust/internet_of_things>
-  PREFIX ex: <http://www.example.audio/>
-
-  SELECT DISTINCT ?entity1 ?pred ?obj
-  WHERE {
-    ?entity1 ?pred ?obj;
-      musico:plays_genre ex:Pop ;
-      foaf:firstName "Atai" .
-  }
-  ORDER BY ?instrument
-  LIMIT 100
-`;
-  let dataToSend = '';
-const stream = await client.query.select(query);
-stream.on('data', (data) => {
-  Object.keys(data).forEach((key) => {
-    dataToSend += `${key}: ${data[key].value} `;
+async function queryData(query) {
+  const stream = await client.query.select(query);
+  return new Promise((resolve, reject) => {
+    var data = [];
+    stream.on("data", (row) => {
+      data.push(row);
+    });
+    stream.on("end", () => {
+      resolve(data);
+    });
+    stream.on("error", (err) => {
+      reject(err);
+    });
   });
+}
+
+app.get("/", (req, res) => {
+  // Redirect to the query page 1
+  res.redirect("/1");
 });
-stream.on('end', () => {
-  res.send(dataToSend);
+app.get("/1", async (req, res) => {
+  res.send(await queryData(SPquery));
 });
 
-
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/2", async (req, res) => {
+  res.send(await queryData(SPquery2));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
